3d0da9d6541b784411bcc7e6497af0dcbe64e093
jonadan <chunsig435@gmail.com>
2025-09-04 17:09:08 +0900

    ccommit

---
+++ study1/SwiftGrammer/forEach.swift
@@ -0,0 +1,5 @@
+var myArray : [Int] = [0,1,2,3,4,5]
+for item in myArray{
+    print("\(myArray)")
+}
+ 
---
+++ study1/SwiftGrammer/if.swift
@@ -0,0 +1,9 @@
+var DarkMode : Bool = true // DarkMode is true
+
+if (DarkMode == true){
+    print("DarkMode")
+}
+if ( DarkMode != true){
+    print("no DarkMode")
+}
+
